3b6d9634a5dc507d7f1823608bfce7bd189371a8
hdong <spdonghao@gmail.com>
2019-03-31 17:29:31 -0400

    add DisplayCategory to DisplayItem

--- PriceCompare/app/src/main/java/ca/uwaterloo/ece651/pricecompare/pricecompare/DisplayCategory.java
+++ PriceCompare/app/src/main/java/ca/uwaterloo/ece651/pricecompare/pricecompare/DisplayCategory.java
@@ -1,7 +1,11 @@
 package ca.uwaterloo.ece651.pricecompare.pricecompare;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ListView;
 import android.widget.SimpleAdapter;
 
@@ -20,7 +24,7 @@ import ca.uwaterloo.ece651.pricecompare.DataReq.http.Api;
 import ca.uwaterloo.ece651.pricecompare.DataReq.http.ApiMethods;
 
 public class DisplayCategory extends AppCompatActivity {
-
+    private String category = "";
 
 
     @Override
@@ -29,10 +33,20 @@ public class DisplayCategory extends AppCompatActivity {
         setContentView(R.layout.activity_display_category);
         // get different labels from 6 different category intents
         Bundle bundle = this.getIntent().getExtras();
-        //  Category is the flag to get diffenent items from daatabase
-        int Category = bundle.getInt("Category");
+        //  Category is the flag to get different items from database
+        int categoryInt = bundle.getInt("Category");
+
+        switch(categoryInt) {
+            case 0: category = "Entertainment";break;
+            case 1: category = "Food";break;
+            case 2: category = "Drink";break;
+            case 3: category = "Home";break;
+            case 4: category = "Wellness";break;
+            case 5: category = "Office";
+        }
 
         ArrayList<HashMap<String, Object>> users = new ArrayList<HashMap<String, Object>>();
+        HashMap<String,String> nameToUPC = new HashMap<>();
         ObserverOnNextListener<List<RecommendationCategory>> RecommandationCategory = recommendationCategories -> {
 
             for (int i = 0; i < 10; i++) {
@@ -40,6 +54,7 @@ public class DisplayCategory extends AppCompatActivity {
                 user.put("product", String.valueOf(recommendationCategories.get(i).getName()));
                 user.put("price", String.valueOf(recommendationCategories.get(i).getPrice()));
                 user.put("store", String.valueOf(recommendationCategories.get(i).getStorename()));
+                nameToUPC.put(String.valueOf(recommendationCategories.get(i).getName()),String.valueOf(recommendationCategories.get(i).getUPC()));
                 users.add(user);
             }
 
@@ -50,9 +65,27 @@ public class DisplayCategory extends AppCompatActivity {
                     // 分别对应view 的id
                     new int[]{R.id.product, R.id.price, R.id.category});
             // 获取listview
-            ((ListView) findViewById(R.id.users)).setAdapter(saImageItems);
+            ListView usersListview =  ((ListView) findViewById(R.id.users));
+            usersListview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                @Override
+                public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                    Object listItem = usersListview.getItemAtPosition(position);
+                    String UPC = listItem.toString().replaceFirst(".*?product=", "");
+                    UPC = UPC.replaceFirst(",\\s+price=.*","");
+                    UPC = nameToUPC.get(UPC);
+                    Log.d("UPC",UPC);
+                    Log.d("item",listItem.toString());
+                    Log.d("category",category);
+                    Intent intent = new Intent(DisplayCategory.this, DisplayItem.class);
+                    intent.putExtra("upc", UPC);
+                    intent.putExtra("activity", "scanner");
+                    intent.putExtra("category",category);
+                    startActivity(intent);
+                }
+            });
+            usersListview.setAdapter(saImageItems);
         };
-        ApiMethods.getRecommendationByCategory(new MyObserver<>(this, RecommandationCategory), Category);
+        ApiMethods.getRecommendationByCategory(new MyObserver<>(this, RecommandationCategory), categoryInt);
 
     }
 
